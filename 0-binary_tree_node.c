dc9a96a8b831ef74bf47e758ab3cdf6bebb0aeb5
rkbrian <rkbriankong@gmail.com>
2021-10-04 02:22:28 +0000

    minor fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,5 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	newnode->n = value;
 	newnode->parent = parent;
+	newnode->left = NULL, newnode->right = NULL;
 	return (newnode);
 }
